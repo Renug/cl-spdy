37e2358d2ee7c28ede57cc7807f292e0f92e5cf3
Renug <liang.rannger@gmail.com>
2013-03-01 17:50:23 +0800

    a
    a

--- spdy-stream.lisp
+++
@@ -1,2 +0,0 @@
-(defclass spdy-stream ()
-  ())
--- stream.lisp
+++ stream.lisp
@@ -12,5 +12,29 @@
    (unidirectional :type boolean
                    :initarg :unidirectional
                    :initform 0
-                   :accessor unidirectional)))
+                   :accessor unidirectional)
+   (input-stream :initarg :input-stream
+                 :type data-input-stream
+                 :accessor input-stream)
+   (output-stream :initarg :output-stream
+                  :type data-output-stream
+                  :accessor output-stream)
+   (frames :initarg :frames
+           :initform #()
+           :type vector)))
+
+(defun mk-spdy-stream (stream-id priority &key 
+                                 (associated-to-stream-id 0) 
+                                 (unidirectional 0)
+                                 (input-stream nil input-stream-p)
+                                 (output-stream nil output-stream-p))
+  (assert (or input-stream-p output-stream-p))
+  (make-instance 'spdy-stream 
+                 :stream-id stream-id
+                 :priority priority
+                 :associated-to-stream-id associated-to-stream-id
+                 :unidirectional unidirectional
+                 :input-stream input-stream
+                 :output-stream output-stream))
+
 
