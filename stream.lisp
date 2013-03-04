656818d5b58b94f2dd7cf2fccda90fabeb700cf1
Renug <liang.rannger@gmail.com>
2013-03-04 17:56:54 +0800

    v
    v

--- stream.lisp
+++ stream.lisp
@@ -1,5 +1,44 @@
 (defparameter *stream-id-count* 0)
 
+
+(defparameter *is-client* t)
+
+(defparameter *server-stream-ids* (make-array 10 
+                                              :element-type '(unsigned-byte 32)
+                                              :adjustable t
+                                              :initial-element 0))
+
+(defparameter *client-stream-ids* (make-array 10 
+                                              :element-type '(unsigned-byte 32)
+                                              :adjustable t
+                                              :initial-element 0))
+
+(defun add-id-to-ids-array (id)
+  (assert (or (= 0 id) 
+              (member id *client-stream-ids*) 
+              (member id *server-stream-ids*)))
+  (if (oddp id)
+      (vector-push id *client-stream-ids*)
+    (vector-push id *server-stream-ids*))
+  id)
+
+(defun generate-server-stream-id ()
+  (if (= 0 (length *server-stream-ids*))
+      (add-id-to-ids-array 2) 
+    (add-id-to-ids-array (+ (aref *server-stream-ids* 
+                                  (- (length *server-stream-ids*) 1)) 2))))
+      
+(defun generate-client-stream-id ()
+  (if (= 0 (length *client-stream-ids*))
+      (add-id-to-ids-array 1)
+    (add-id-to-ids-array (+ 2 (aref *client-stream-ids* 
+                                    (- (length *server-stream-ids*) 1))))))
+
+(defun generate-stream-id ()
+  (if *is-client*
+      (generate-server-stream-id)
+    (generate-client-stream-id)))
+
 (defclass spdy-stream ()
   ((stream-id :initarg :stream-id
               :initform 0
@@ -18,11 +57,12 @@
                  :type data-input-stream
                  :accessor input-stream)
    (output-stream :initarg :output-stream
-                  :type data-output-stream
                   :accessor output-stream)
-   (frames :initarg :frames
-           :initform #()
-           :type vector)))
+   (input-frames :initarg :input-frames
+                 :initform (make-array 10 :adjustable t))
+   (output-frames :initarg :output-frames
+                  :initform (make-array 10 :adjustable t)
+                  :accessor output-frames)))
 
 (defun mk-spdy-stream (stream-id priority &key 
                                  (associated-to-stream-id 0) 
@@ -37,5 +77,14 @@
                  :unidirectional unidirectional
                  :input-stream input-stream
                  :output-stream output-stream))
+
+(defmethod flush-frames ((stream spdy-stream))
+  (with-slots (output-frames output-stream) stream
+    (loop for frame across output-frames do
+          (let ((result-array (frame-to-list frame)))
+            (loop for byte across result-array do
+                  (write-byte byte output-stream))))
+    (loop repeat (array-total-size output-frames) do
+          (vector-pop output-frames))))
 
 
